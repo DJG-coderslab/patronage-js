39c07cbee1c0cd6fadf97037fd6558d0c59d6c88
DJG-coderslab <coderslab.sjg@pwr.pl>
2021-12-06 19:46:34 +0100

    The form add radio and multiselect

--- src/components/EditUser/EditUser.tsx
+++ src/components/EditUser/EditUser.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Controller, useForm } from "react-hook-form";
 import {
   MDBBtn,
   MDBModal,
@@ -15,19 +16,31 @@ import { setShowEditUserWindow, setUserToEdit } from "../../redux/actions";
 import useInput from "./../../hooks/useInput";
 import userType from "../../types/user";
 import { Form } from "react-bootstrap";
+import * as R from "ramda";
 
 export default function EditUser() {
   const dispatch = useDispatch();
-  const { showEditUserWindow } = useSelector((state) => state.userReducer);
+  const { hobbies, showEditUserWindow } = useSelector(
+    (state) => state.userReducer
+  );
   const u = useSelector((state) => state.userReducer.userToEdit);
-  const [name, connectName, setName] = useInput(u.name);
-  const [lastName, connectLastName, setLastName] = useInput(u.lastName);
-  const [email, connectEmail, setEmail] = useInput(u.email);
-  const [address, connectAddress, setAddress] = useInput(u.address);
-  const [birthday, connectBirthday, setBirthday] = useInput(u.dateOfBirth);
-  const [phone, connectPhone, setPhone] = useInput(u.phoneNumber);
-  const [gender, connectGender, setGender] = useInput(u.gender);
-  const [hobbies, connectHobbies, setHobbies] = useInput("");
+  // const [name, connectName, setName] = useInput(u.name);
+  // const [lastName, connectLastName, setLastName] = useInput(u.lastName);
+  // const [email, connectEmail, setEmail] = useInput(u.email);
+  // const [address, connectAddress, setAddress] = useInput(u.address);
+  // const [birthday, connectBirthday, setBirthday] = useInput(u.dateOfBirth);
+  // const [phone, connectPhone, setPhone] = useInput(u.phoneNumber);
+  // const [gender, connectGender, setGender] = useInput(u.gender);
+  // const [hobbies, connectHobbies, setHobbies] = useInput("");
+
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
   const closeWindow = () => {
     dispatch(setShowEditUserWindow(false));
     // dispatch(setUserToEdit(""));
@@ -35,92 +48,150 @@ export default function EditUser() {
 
   useEffect(() => {
     console.log("U was changed ");
-    setName(u.name);
-    setLastName(u.lastName);
-    setEmail(u.email);
-    setAddress(u.address);
-    setBirthday(u.dateOfBirth);
-    setPhone(u.phoneNumber);
-    setGender(u.gender);
+    // setName(u.name);
+    // setLastName(u.lastName);
+    // setEmail(u.email);
+    // setAddress(u.address);
+    // setBirthday(u.dateOfBirth);
+    // setPhone(u.phoneNumber);
+    // setGender(u.gender);
+    setValue("name", u.name);
+    setValue("lastName", u.lastName);
+    setValue("email", u.email);
+    setValue("address", u.address);
+    setValue("phone", u.phoneNumber);
+    setValue("dateOfBirth", u.dateOfBirth);
+    setValue("gender", u.gender);
+    setValue("hobbies", u.hobbies);
   }, [u]);
+
+  const onSubmit = (data) => console.log("Data: ", data);
+  console.log("Errors: ", errors);
+
+  const selecty = (w2Hobbies, usrHobbies) => {};
 
   return (
     <MDBModal show={showEditUserWindow} staticBackdrop tabIndex="-1">
-      <MDBModalDialog>
-        <MDBModalContent>
-          <MDBModalHeader>
-            <MDBModalTitle>Edit user</MDBModalTitle>
-            <MDBBtn className="btn-close" color="none" onClick={closeWindow} />
-          </MDBModalHeader>
-          <MDBModalBody></MDBModalBody>
-          <Form>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <MDBModalDialog>
+          <MDBModalContent>
+            <MDBModalHeader>
+              <MDBModalTitle>Edit user</MDBModalTitle>
+              <MDBBtn
+                className="btn-close"
+                color="none"
+                onClick={closeWindow}
+              />
+            </MDBModalHeader>
+            <MDBModalBody></MDBModalBody>
+
+            {/* <Controller
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <Form.Control
+                  type="text"
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+              name="firstName"
+            /> */}
+
             <Form.Group className="mb-3 px-3" controlId="formEditUser">
               <Form.Label>Name</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter your name"
-                {...connectName}
-                value={name}
+                {...register("name", { required: true })}
               />
               <Form.Label>Last Name</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter your last name"
-                {...connectLastName}
-                value={lastName}
+                {...register("lastName")}
               />
               <Form.Label>Email</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter your email"
-                {...connectEmail}
-                value={email}
+                {...register("email")}
               />
               <Form.Label>Gender</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your gender"
-                {...connectGender}
-                value={gender}
-              />
+              <Form.Group>
+                <Form.Check
+                  id="maleID"
+                  // name="gender"
+                  inline
+                  label="male"
+                  type="radio"
+                  value="male"
+                  {...register("gender")}
+                  // checked={gender === "male" ? true : false}
+                />
+                <Form.Check
+                  id="femaleID"
+                  // name="gender"
+                  {...register("gender")}
+                  value="female"
+                  inline
+                  label="female"
+                  type="radio"
+                  // checked={gender === "female" ? true : false}
+                />
+                <Form.Check
+                  id="nothingID"
+                  // name="gender"
+                  {...register("gender")}
+                  value="doesn'tMatter"
+                  inline
+                  label="doesn't matter"
+                  type="radio"
+                  // checked={gender === "doesentmatter" ? true : false}
+                />
+              </Form.Group>
               <Form.Label>Phone number</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter your phone number"
-                {...connectPhone}
-                value={phone}
+                {...register("phone")}
               />
               <Form.Label>Address</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter your address"
-                {...connectAddress}
-                value={address}
+                {...register("address")}
               />
               <Form.Label>Date of birdth</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter your date of birth"
-                {...connectBirthday}
-                value={birthday}
+                {...register("dateOfBirth")}
               />
               <Form.Label>Hobbies</Form.Label>
-              <Form.Control
-                type="text"
+              <Form.Select
                 placeholder="Enter your hobbies"
-                {...connectHobbies}
-                value={hobbies}
-              />
+                multiple
+                {...register("hobbies")}
+              >
+                {R.pipe(
+                  R.innerJoin((hobby, userHobby) => hobby.id === userHobby),
+                  R.map((hobby) => (
+                    <option key={hobby.id} value={hobby.name}>
+                      {hobby.name}
+                    </option>
+                  ))
+                )(hobbies, u.hobbies)}
+              </Form.Select>
             </Form.Group>
-          </Form>
-          <MDBModalFooter>
-            <MDBBtn color="secondary" onClick={closeWindow}>
-              Cancel
-            </MDBBtn>
-            <MDBBtn color="success">Send it</MDBBtn>
-          </MDBModalFooter>
-        </MDBModalContent>
-      </MDBModalDialog>
+            <MDBModalFooter>
+              <MDBBtn color="secondary" onClick={closeWindow}>
+                Cancel
+              </MDBBtn>
+              <MDBBtn color="success">Send it</MDBBtn>
+            </MDBModalFooter>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </Form>
     </MDBModal>
     // TODO needed the agree to leave unsave Form
   );
